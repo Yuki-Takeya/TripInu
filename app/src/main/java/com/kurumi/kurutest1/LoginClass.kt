a1e0a3c1f9e5ae2b26bc6e6f6f4497d2264f6546
kurumi <crazyboys908@gmail.com>
2021-12-07 03:22:32 +0900

    Initial commit

---
+++ app/src/main/java/com/kurumi/kurutest1/LoginClass.kt
@@ -0,0 +1,17 @@
+package com.kurumi.kurutest1
+
+import com.kurumi.kurutest1.R
+
+import android.os.Bundle
+
+
+
+
+class LoginClass : MainActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login_page)
+    }
+
+}
